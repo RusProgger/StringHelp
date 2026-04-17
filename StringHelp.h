efcfb6b02a9d0af8f470622de0ca5d360e8a9ab4
RusProgger <ruslantimka92@gmail.com>
2026-04-17 13:46:34 +0300

    Work string

---
+++ StringHelp.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+// UpperCase
+
+inline std::string ToUpper(std::string text)
+{
+    std::transform(text.begin(), text.end(), text.begin(),
+        [](unsigned char c) {
+            return std::toupper(c);
+        });
+
+    return text;
+}
+
+
+// LowerCase
+
+inline std::string ToLower(std::string text)
+{
+    std::transform(text.begin(), text.end(), text.begin(),
+        [](unsigned char c) {
+            return std::tolower(c);
+        });
+
+    return text;
+}
+
+// Capitalize
+
+inline std::string Capitalize(std::string text)
+{
+    if (text.empty())
+        return text;
+
+    
+    std::transform(text.begin(), text.end(), text.begin(),
+        [](unsigned char c) {
+            return std::tolower(c);
+        });
+
+    text[0] = std::toupper(text[0]);
+
+    return text;
+}
